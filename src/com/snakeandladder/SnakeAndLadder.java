f00e275a1274140461346c05dd60d1cd378e766d
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-23 13:49:56 +0530

    ADDED: Snake And Ladder Game and set starting postion 0

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,9 +1,13 @@
 package com.snakeandladder;
 
+import com.snakeandladder.SnakeAndLadderGame;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder problem");
+		SnakeAndLadderGame snakeAndLadderGame = new SnakeAndLadderGame();
+		
 	}
 
 }
---
+++ src/com/snakeandladder/SnakeAndLadderGame.java
@@ -0,0 +1,10 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderGame {
+	public static int playerPosition;
+	
+	SnakeAndLadderGame() {
+		this.playerPosition = 0;
+		System.out.println("Snake and Ladder game starts player position is "+this.playerPosition);
+	}
+}
