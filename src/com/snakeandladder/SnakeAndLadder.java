9e64c2fd3bb3cc65bbe630282dadb2f09c3989a9
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-23 14:03:20 +0530

    ADDED: Dice to Snake And Ladder Game

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -7,7 +7,7 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder problem");
 		SnakeAndLadderGame snakeAndLadderGame = new SnakeAndLadderGame();
-		
+		System.out.println("Player roll dice and got "+snakeAndLadderGame.dice());
 	}
 
 }
--- src/com/snakeandladder/SnakeAndLadderGame.java
+++ src/com/snakeandladder/SnakeAndLadderGame.java
@@ -7,4 +7,8 @@ public class SnakeAndLadderGame {
 		this.playerPosition = 0;
 		System.out.println("Snake and Ladder game starts player position is "+this.playerPosition);
 	}
+	
+	public int dice() {
+		return (int) Math.floor(Math.random() * 10) % 6 + 1;
+	}
 }
