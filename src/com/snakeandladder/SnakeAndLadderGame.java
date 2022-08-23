6c7b161f7feaa1fb7e662928f53f573551c714ee
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-23 14:24:10 +0530

    ADDED: Play Option to game (No Play, Ladder, Snake)

--- src/com/snakeandladder/SnakeAndLadderGame.java
+++ src/com/snakeandladder/SnakeAndLadderGame.java
@@ -1,6 +1,8 @@
 package com.snakeandladder;
 
 public class SnakeAndLadderGame {
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
 	public static int playerPosition;
 	
 	SnakeAndLadderGame() {
@@ -11,4 +13,21 @@ public class SnakeAndLadderGame {
 	public int dice() {
 		return (int) Math.floor(Math.random() * 10) % 6 + 1;
 	}
+	
+	public void play() {
+		int crrDice = this.dice();
+		int playOption = (int) Math.floor(Math.random() * 10) % 3;
+		switch (playOption) {
+			case LADDER:
+				System.out.println("Play option is Ladder the player moves ahead by the "+crrDice+" number of position");
+				break;
+			case SNAKE:
+				System.out.println("Play option is Snake the player moves behind by the "+crrDice+" number of position");
+				break;
+			default:
+				System.out.println("Play option is No Play the player stays in the same position");
+		}
+	}
+	
+	
 }
